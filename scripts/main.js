408c80c5da24df73dfabb1fe32c9a23216bb2250
Srijan Nayak <srijannayak0@gmail.com>
2021-08-13 11:22:54 +0530

    feat: Plan out the initial algorithm

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-console.log("Script connected");
+// 1. declare and initialize playerWins and computerWins to 0
+// 2. play 5 rounds by repeating the following 5 times:
+//   1. get computer's play and store it in computerSelection
+//   2. get user's play and store it in playerSelection
+//   3. decide winner by comparing computerSelection and playerSelection
+//      and store it in winner
+//   4. if winner is player:
+//     1. increment playerWins
+//   5. else if winner is computer:
+//     1. increment computerWins
+// 3. if playerWins is greater than computerWins:
+//   1. Declare player as winner
+// 4. else if computerWins is greater than computerWins:
+//   1. Declare player as loser
+// 5. else:
+//   1. declare tie
