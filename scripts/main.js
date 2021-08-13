adbb8016d2efb7ae3af42341d8d92d92cee5cdf1
Srijan Nayak <srijannayak0@gmail.com>
2021-08-13 12:45:41 +0530

    style: Move function definitions to the bottom

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,13 @@
 const computerSelection = computerPlay();
 //   2. get user's play and store it in playerSelection
 const playerSelection = playerPlay();
+//   3. decide winner by comparing computerSelection and playerSelection
+//      and store it in winner
+//   4. if winner is player increment playerWins
+//   5. if winner is computer increment computerWins
+// 3. if playerWins is greater than computerWins declare player as winner
+// 4. if computerWins is greater than computerWins declare player as loser
+// 5. else declare tie
 
 function computerPlay() {
   const randomNumber = Math.floor(Math.random() * 3);
@@ -26,11 +33,3 @@ function playerPlay() {
   }
   return playerInput;
 }
-
-//   3. decide winner by comparing computerSelection and playerSelection
-//      and store it in winner
-//   4. if winner is player increment playerWins
-//   5. if winner is computer increment computerWins
-// 3. if playerWins is greater than computerWins declare player as winner
-// 4. if computerWins is greater than computerWins declare player as loser
-// 5. else declare tie
