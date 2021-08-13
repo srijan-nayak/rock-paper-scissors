be880bf96cfed2b9d1f79c1a83df030744ae5ce2
Srijan Nayak <srijannayak0@gmail.com>
2021-08-13 18:10:50 +0530

    feat: Get result of round by comparing player's and computer's move

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,10 @@ const computerSelection = computerPlay();
 const playerSelection = playerPlay();
 //   3. decide winner by comparing computerSelection and playerSelection
 //      and store it in winner
+const result = playRound(playerSelection, computerSelection);
+console.log(`Computer played ${computerSelection}`);
+console.log(`You played ${playerSelection}`);
+console.log(result);
 //   4. if winner is player increment playerWins
 //   5. if winner is computer increment computerWins
 // 3. if playerWins is greater than computerWins declare player as winner
@@ -32,4 +36,39 @@ function playerPlay() {
     return playerPlay();
   }
   return playerInput;
+}
+
+function playRound(playerSelection, computerSelection) {
+  const PLAYER_WIN_MESSAGE = "You win!";
+  const PLAYER_LOSE_MESSAGE = "You lose!";
+  const TIE_MESSAGE = "Tie! Both played the same move";
+  const ROCK_WIN_REASON = "Rock beats Scissors";
+  const PAPER_WIN_REASON = "Paper beats Rock";
+  const SCISSORS_WIN_REASON = "Scissors beats Paper";
+
+  if (playerSelection === "rock") {
+    if (computerSelection === "rock") {
+      return `${TIE_MESSAGE}`;
+    } else if (computerSelection === "paper") {
+      return `${PLAYER_LOSE_MESSAGE} ${PAPER_WIN_REASON}`;
+    } else if (computerSelection === "scissors") {
+      return `${PLAYER_WIN_MESSAGE} ${ROCK_WIN_REASON}`;
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      return `${PLAYER_WIN_MESSAGE} ${PAPER_WIN_REASON}`;
+    } else if (computerSelection === "paper") {
+      return `${TIE_MESSAGE}`;
+    } else if (computerSelection === "scissors") {
+      return `${PLAYER_LOSE_MESSAGE} ${SCISSORS_WIN_REASON}`;
+    }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      return `${PLAYER_LOSE_MESSAGE} ${ROCK_WIN_REASON}`;
+    } else if (computerSelection === "paper") {
+      return `${PLAYER_WIN_MESSAGE} ${SCISSORS_WIN_REASON}`;
+    } else if (computerSelection === "scissors") {
+      return `${TIE_MESSAGE}`;
+    }
+  }
 }
